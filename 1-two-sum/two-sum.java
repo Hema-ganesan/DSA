421302fa0301aca1cafc30ff5de31029b86f04c2
Hemalatha G <g.hema04.05.2006@gmail.com>
2025-12-15 18:14:23 +0530

    Time: 2 ms (99.10%) | Memory: 47 MB (26.67%) - LeetSync

---
+++ 1-two-sum/two-sum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        java.util.HashMap<Integer, Integer> map = new java.util.HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int rem = target - nums[i];
+            if (map.containsKey(rem)) {
+                return new int[]{map.get(rem), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+}
