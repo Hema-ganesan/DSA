c2334216e5100550f94d65a152f43cb754586b81
Hemalatha G <g.hema04.05.2006@gmail.com>
2026-01-24 14:19:36 +0530

    Time: 6 ms (84.49%) | Memory: 46.9 MB (76.98%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -3,6 +3,9 @@ class Solution {
         int[] count = new int[26];
         for (int i = 0; i < s.length(); i++) {
             count[s.charAt(i) - 'a']++;
+            //the "s.charAt(i)-'a' used for index calculation
+            //eg: if a at ith pos then 97 - 97=0(index 0)
+            //if b at ith pos then 98-97=1(index 1)
         }
         for (int i = 0; i < s.length(); i++) {
             if (count[s.charAt(i) - 'a'] == 1) {
