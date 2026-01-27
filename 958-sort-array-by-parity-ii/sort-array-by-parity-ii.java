c8eb6e2ead749c54ae8157991105f35f76f78361
Hemalatha G <g.hema04.05.2006@gmail.com>
2026-01-27 13:45:22 +0530

    Time: 2 ms (99.32%) | Memory: 49.4 MB (7.32%) - LeetSync

---
+++ 958-sort-array-by-parity-ii/sort-array-by-parity-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] sortArrayByParityII(int[] nums) {
+        int ev = 0, od = 1;  
+        int[] res = new int[nums.length];  
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] % 2 == 0) {
+                res[ev] = nums[i];
+                ev += 2;
+            } else {
+                res[od] = nums[i];
+                od += 2;
+            }
+        }
+        return res;
+    }
+}
