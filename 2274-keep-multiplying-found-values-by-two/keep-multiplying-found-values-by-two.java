d3572d48b330a99fd47a18d97cb96d62f401f53d
Hemalatha G <g.hema04.05.2006@gmail.com>
2026-02-03 22:04:55 +0530

    Time: 4 ms (47.06%) | Memory: 46.5 MB (22.74%) - LeetSync

--- 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.java
+++ 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.java
@@ -10,35 +10,35 @@
 //     }
 // }
 
-class Solution {
-    public int findFinalValue(int[] nums, int original) {
-        int found = 1;
-        while (found == 1) {
-            found = 0;
-            for (int i = 0; i < nums.length; i++) {
-                if (nums[i] == original) {
-                    original = original * 2;
-                    found = 1;
-                    break;
-                }
-            }
-        }
-        return original;
-    }
-}
-
-
 // class Solution {
 //     public int findFinalValue(int[] nums, int original) {
-//        HashSet<Integer> s=new HashSet<>();
-//        for(int i=0;i<nums.length;i++)
-//        {
-//         s.add(nums[i]);
-//        }
-//        while(s.contains(original))
-//        {
-//             original=2*original;
-//        }
-//        return original;
+//         int found = 1;
+//         while (found == 1) {
+//             found = 0;
+//             for (int i = 0; i < nums.length; i++) {
+//                 if (nums[i] == original) {
+//                     original = original * 2;
+//                     found = 1;
+//                     break;
+//                 }
+//             }
+//         }
+//         return original;
 //     }
 // }
+
+
+class Solution {
+    public int findFinalValue(int[] nums, int original) {
+       HashSet<Integer> s=new HashSet<>();
+       for(int i=0;i<nums.length;i++)
+       {
+        s.add(nums[i]);
+       }
+       while(s.contains(original))
+       {
+            original=2*original;
+       }
+       return original;
+    }
+}
