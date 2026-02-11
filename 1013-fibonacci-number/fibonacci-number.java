b6b91593cf33dd2dedaea104201116d0ccf6f913
Hemalatha G <g.hema04.05.2006@gmail.com>
2026-02-11 22:02:59 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (13.58%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int fib(int n) {
+        if(n==0) 
+            return 0;
+        if(n==1) 
+            return 1;  
+        int f2=0;
+        int f1=1;
+        for(int i=2;i<=n;i++){
+            int current=f1+f2;
+            f2=f1;
+            f1=current;
+        }
+        return f1;
+    }
+}
