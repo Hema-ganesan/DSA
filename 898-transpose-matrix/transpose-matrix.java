181fd6bfb497cd4605472d166080ca770d3482cd
Hemalatha G <g.hema04.05.2006@gmail.com>
2026-05-17 19:46:45 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (66.81%) - LeetSync

---
+++ 898-transpose-matrix/transpose-matrix.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        int m=matrix.length,n=matrix[0].length;
+        int[][] bh=new int[n][m];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                bh[i][j]=matrix[j][i];
+            }
+        }
+        return bh;
+    }
+}
