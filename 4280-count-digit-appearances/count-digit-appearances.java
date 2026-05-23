dc3299d6383c5925f212b0a03fd8b3ff82b6a1c4
Hemalatha G <g.hema04.05.2006@gmail.com>
2026-05-23 09:06:29 +0530

    Time: 4 ms (98.01%) | Memory: 46.9 MB (82.20%) - LeetSync

--- 4280-count-digit-appearances/count-digit-appearances.java
+++ 4280-count-digit-appearances/count-digit-appearances.java
@@ -3,9 +3,9 @@ class Solution {
         int cnt=0;
         for (int i=0;i<nums.length;i++){
             int n=nums[i];
-            // if(n==0&&digit==0) {
-            //     cnt++;
-            // }
+            if(n==0&&digit==0) {
+                cnt++;
+            }
             while(n>0){
                 if (n%10==digit){
                     cnt++;
