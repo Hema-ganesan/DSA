8a99ad18611c9a556280d3dc7bd445051c13b1e0
Hemalatha G <g.hema04.05.2006@gmail.com>
2026-06-06 11:23:21 +0530

    Time: 18 ms (5.91%) | Memory: 46.7 MB (13.15%) - LeetSync

--- 2714-left-and-right-sum-differences/left-and-right-sum-differences.java
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.java
@@ -1,45 +1,19 @@
-// class Solution {
-//     public int[] leftRightDifference(int[] nums) {
-//         int[] leftsum=new int[nums.length];
-//         int[] rightsum=new int[nums.length];
-//         int[] ans=new int[nums.length];
-//         int k=0;
-//         for(int i=0;i<nums.length;i++){
-//             if(nums[i-1]==0){
-//                 leftsum[i]=0;
-//                 for(int j=i+1;i<nums.length;i++){
-//                     rightsum[i]=nums[j];
-//                     ans[k++]=Math.abs(leftsum[i]-rightsum[k]);
-//                 }
-//             }
-//             else{
-//                 leftsum[i]=nums[i];
-//             }
-//         }
-//     }
-// }
-
 class Solution {
     public int[] leftRightDifference(int[] nums) {
-
-        int n = nums.length;
-
-        int[] leftsum = new int[n];
-        int[] rightsum = new int[n];
-        int[] ans = new int[n];
-
-        for (int i = 1; i < n; i++) {
-            leftsum[i] = leftsum[i - 1] + nums[i - 1];
+        int[] leftsum=new int[nums.length];
+        int[] rightsum=new int[nums.length];
+        int[] ans=new int[nums.length];
+        int k=0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<i;j++){
+                leftsum[i]+=nums[j];
+            }
+            for(int j=i+1;j<nums.length;j++){
+                rightsum[i]+=nums[j];
+            }
+            ans[i]=Math.abs(leftsum[i]-rightsum[i]);
         }
-
-        for (int i = n - 2; i >= 0; i--) {
-            rightsum[i] = rightsum[i + 1] + nums[i + 1];
-        }
-
-        for (int i = 0; i < n; i++) {
-            ans[i] = Math.abs(leftsum[i] - rightsum[i]);
-        }
-
-        return ans;
+        return ans; 
     }
 }
+
