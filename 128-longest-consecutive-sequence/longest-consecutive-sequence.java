a6e4f1cba4df6cbc74b07daa54510c8729d08dbd
Hemalatha G <g.hema04.05.2006@gmail.com>
2026-07-16 20:14:55 +0530

    Time: 35 ms (19.87%) | Memory: 95.7 MB (27.89%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -4,23 +4,22 @@ class Solution {
             return 0;
         }
         HashSet<Integer> h=new HashSet<>();
-        int c=0,l=1;
         for(int i=0;i<nums.length;i++){
             h.add(nums[i]);
         }
+        int c=0,l=1;
         for(int i:h){
             if(!h.contains(i-1)){
                 int x=i;
                 c=1;
                 while(h.contains(x+1)){
                     c++;
-                    x=x+1;
-                }
-                if(c>l){
-                    l=c;
+                    x+=1;
                 }
             }
+             l=Math.max(l,c);
         }
+       
         return l;
     }
 }
