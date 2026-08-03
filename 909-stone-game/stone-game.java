b6075e425a11e425823d16b2734e9192bb3f5140
Hemalatha G <g.hema04.05.2006@gmail.com>
2026-08-03 19:02:37 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (37.02%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,12 +1,5 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        int sum=0;
-        for(int i=0;i<piles.length;i++){
-            sum+=piles[i];
-        }
-        if(sum%2==0){
-            return false;
-        }
         return true;
     }
 }
