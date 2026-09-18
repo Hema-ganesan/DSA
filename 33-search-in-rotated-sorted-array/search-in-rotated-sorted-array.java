e868901e9d16fef58c2c15664545eb9825dc24c8
Hemalatha G <g.hema04.05.2006@gmail.com>
2026-09-18 12:04:24 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (11.72%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -3,26 +3,26 @@ class Solution {
         int l=0,h=nums.length-1;
         while(l<=h){
             int m=l+(h-l)/2;
-            if(nums[m]==target)
+            if(nums[m]==target){
                 return m;
-            
-            if(nums[l]<=nums[m]){
-                if(nums[l]<=target && target<=nums[m]){
+            }
+            else if(nums[l]<=nums[m]){
+                if(nums[l]<=target && target<nums[m]){
                     h=m-1;
                 }
                 else{
                     l=m+1;
                 }
             }
-
             else{
-                if(nums[m]<=target && target<=nums[h]){
+                if(nums[m]<=target && nums[h]>=target){
                     l=m+1;
                 }
-                else
+                else{
                     h=m-1;
+                }
             }
         }
         return -1;
-    }
+    }        
 }
